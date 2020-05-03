a95799e33057a2569beaef95fdb216bbfa7c7728
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 04:03:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -126,4 +126,16 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
