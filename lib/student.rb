40c76e3b467771d279d77649d755aa999556571a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 03:45:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,3 +64,15 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
+def self.all_students_in_grade_9
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+  SQL
+
+  DB[:conn].execute(sql,grade).collect do |row|
+    self.new_from_db(row)
+  end
+end
