5a148be962f85595ad00b7660a2d691060352bd1
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 03:40:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,10 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    binding.pry
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
   end
 
   def self.all
