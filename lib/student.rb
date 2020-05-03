6032fa51f994b76a52702a4bba30cbe1c38fc9f3
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-03 03:59:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -113,6 +113,17 @@ class Student
     end
   end
 
-  def
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
 
 end
